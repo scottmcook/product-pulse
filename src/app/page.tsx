98db26a0954eace735f18674cdd2843b017098f3
scottmcook <scott.madison.cook@gmail.com>
2023-08-16 19:59:49 -0700

    Add list items organizing products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,14 @@ async function getData() {
 
 export default async function Home() {
   const res = await fetch('https://static.ui.com/fingerprint/ui/public.json');
-  const data:Devices = await res.json();
-  console.log(data.devices)
+  const data = await res.json();
+  const listItems = data.devices.map((device) =>
+    <li key={device.id}>{device.product.name}</li>
+  );
+
   return (
     <main>
-      <div>{data.devices}</div>
+      <ul>{listItems}</ul>
     </main>
   )
 }
