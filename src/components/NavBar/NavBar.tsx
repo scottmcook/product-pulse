8205688ce38e5174b4705b16f0cab76879f240da
scottmcook <scott.madison.cook@gmail.com>
2023-08-17 22:06:53 -0700

    Update image file directory

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import UbiquitiLogo from '../../assets/ubiquiti-logo.png';
+import UbiquitiLogo from '../../icons/ubiquiti-logo.png';
 
 function NavBar() {
 	return ( 
 		<nav className='flex justify-between items-center font-dark-gray background-color--light'>
 			<div className='flex flex-row items-center'>
-				<Image className='mr-4' src={UbiquitiLogo} alt="Ubiquiti Logo" />
+				<Image className='mr-4' height={55} src={UbiquitiLogo} alt="Ubiquiti Logo" />
 				<div>Devices</div>
 			</div>
 			<div className='justify-self-end pr-8'>Scott Cook</div>
