6064951c64dbdb8e28c0e4a503fe73d5ca2348d9
scottmcook <scott.madison.cook@gmail.com>
2023-08-17 17:11:58 -0700

    Add logo to navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
 export default async function Home() {
   const res = await fetch('https://static.ui.com/fingerprint/ui/public.json');
   const data = await res.json();
-  const listItems = data.devices.map((device: Devices) =>
+  const listItems = data.devices.map((device) =>
     <li key={device.id}>{device.product.name}</li>
   );
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
-import UbiquitiLogo from '../../assets/ubiquitit-logo.png';
+import UbiquitiLogo from '../../assets/ubiquiti-logo.png';
 
 function NavBar() {
 	return ( 
-		<nav className='flex flex-row'>
-			<Image src={UbiquitiLogo} alt="Ubiquiti Logo" />
-			<div>Devices</div>
-			<div className='justify-self-end'>Scott Cook</div>
+		<nav className='flex justify-between items-center font-dark-gray background-color--light'>
+			<div className='flex flex-row items-center'>
+				<Image className='mr-4' src={UbiquitiLogo} alt="Ubiquiti Logo" />
+				<div>Devices</div>
+			</div>
+			<div className='justify-self-end pr-8'>Scott Cook</div>
 		</nav> 
 	);
 }
