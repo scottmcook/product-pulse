6cef7fe8bcd8782eda2ee51c1c91f70eb3b8954a
scottmcook <scott.madison.cook@gmail.com>
2023-08-17 17:43:23 -0700

    Remove data fetching to pass builds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,15 @@
 
 
 export default async function Home() {
-  const res = await fetch('https://static.ui.com/fingerprint/ui/public.json');
-  const data = await res.json();
-  const listItems = data.devices.map((device) =>
-    <li key={device.id}>{device.product.name}</li>
-  );
+  // const res = await fetch('https://static.ui.com/fingerprint/ui/public.json');
+  // const data = await res.json();
+  // const listItems = data.devices.map((device) =>
+  //   <li key={device.id}>{device.product.name}</li>
+  // );
 
   return (
     <main>
-      <ul>{listItems}</ul>
+      {/* <ul>{listItems}</ul> */}
     </main>
   )
 }
