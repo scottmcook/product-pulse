9746a1221f74291b275e1a5ec195b8215ca69f00
scottmcook <scott.madison.cook@gmail.com>
2023-08-17 21:46:10 -0700

    Add border styles

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Toolbar() {
 	return ( 
-		<div className='flex justify-between px-7'>
+		<div className='flex justify-between border border-y-[#ededf0] px-7'>
 			<input className='background-color--light' placeholder='Search' />
 
 			<div>
