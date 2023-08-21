ce9d5c7decd5d08b6cf43208f89206b75d7a4cc8
scottmcook <scott.madison.cook@gmail.com>
2023-08-21 14:35:26 -0700

    Add data fetching to layout component

--- src/components/ProductLayout/ProductLayout.tsx
+++ src/components/ProductLayout/ProductLayout.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import ProductCard from "../ProductCard/ProductCard";
+import ProductTable from "../ProductTable/ProductTable";
+
+// Data fetching
+import useSWR from "swr";
+
+const UIDB_URL = "https://static.ui.com/fingerprint/ui/public.json";
+const fetcher = (UIDB_URL) => fetch(UIDB_URL).then((res) => res.json());
 
 function ProductLayout() {
+  let isTable = false;
+  const { data, isLoading, isError } = useProducts();
+
+  if (isTable) {
+    return (
+      <div className="grid mt-6 mx-14">
+        <ProductTable />
+      </div>
+    );
+  }
   return (
     <>
       <div className="grid grid-cols-5 gap-5 mt-6 mx-14">
-        <ProductCard name={"PlaceholderProductCard"} />
-        <ProductCard name={"PlaceholderProductCard"} />
-        <ProductCard name={"PlaceholderProductCard"} />
-        <ProductCard name={"PlaceholderProductCard"} />
-        <ProductCard name={"PlaceholderProductCard"} />
-        <ProductCard name={"PlaceholderProductCard"} />
         <ProductCard name={"PlaceholderProductCard"} />
       </div>
     </>
