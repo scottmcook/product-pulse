9cf1fb7ec0b0d714cd66113a383d8226184ba0c6
scottmcook <scott.madison.cook@gmail.com>
2023-08-21 21:06:49 -0700

    Add alignt and font styles

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,15 +4,25 @@ import Image from "next/image";
 interface Product {
   name: string;
   imageUrl: string;
+  productType: string;
 }
 
 function ProductCard(props: Product) {
   return (
     <div className="flex flex-col border border-[#dbdce1] h-48 rounded-lg">
-      <div>
-        <Image alt={props.name} height={124} width={100} src={props.imageUrl} />
+      <div className="flex justify-center w-full background-color--light">
+        <Image
+          className=""
+          alt={props.name}
+          height={124}
+          width={100}
+          src={props.imageUrl}
+        />
       </div>
-      <div>{props.name}</div>
+      <div className="mt-3 ml-3">
+        <div>{props.name}</div>
+        <div className="text-sm text-gray-600">{props.productType}</div>
+      </div>
     </div>
   );
 }
