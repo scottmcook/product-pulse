8338e68247fdc4c814f40262248d5eac164d6163
scottmcook <scott.madison.cook@gmail.com>
2023-08-21 21:19:47 -0700

    Update the array type

--- src/components/ProductLayout/ProductLayout.tsx
+++ src/components/ProductLayout/ProductLayout.tsx
@@ -10,7 +10,7 @@ function ProductLayout() {
   let isTable = false;
 
   const UIDB_URL = "https://static.ui.com/fingerprint/ui/public.json";
-  const UIDB_IMAGE_URL = "https://static.ui.com/fingerprint/ui";
+  const UIDB_IMAGE_URL = "https://static.ui.com/fingerprint/ui/icons";
   const fetcher = async (url: RequestInfo | URL) =>
     fetch(url).then((res) => res.json());
   const { data, error } = useSWR(UIDB_URL, fetcher);
@@ -45,13 +45,13 @@ function ProductLayout() {
               id: React.Key | null | undefined;
               product: { name: string };
               line: { name: string };
-              icon: { resolutions: number[]; id: string };
+              icon: { resolutions: any[]; id: string };
             }) => (
               <ProductCard
                 key={device.id}
                 name={device.product.name}
                 productType={device.line.name}
-                imageUrl={`${UIDB_IMAGE_URL}/icons/${device.icon.id}_${device.icon.resolutions[0][0]}x${device.icon.resolutions[0][0]}.png`}
+                imageUrl={`${UIDB_IMAGE_URL}/${device.icon.id}_${device.icon.resolutions[3][0]}x${device.icon.resolutions[3][0]}.png`}
               />
             )
           )}
