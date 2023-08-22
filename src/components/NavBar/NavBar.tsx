713016c270009fb95dd573415ec10321ab11bae7
scottmcook <scott.madison.cook@gmail.com>
2023-08-22 15:58:23 -0700

    Update font color

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
-import UbiquitiLogo from '../../icons/ubiquiti-logo.png';
+import React from "react";
+import Image from "next/image";
+import UbiquitiLogo from "../../icons/ubiquiti-logo.png";
 
 function NavBar() {
-	return ( 
-		<nav className='flex justify-between items-center font-dark-gray background-color--light'>
-			<div className='flex flex-row items-center'>
-				<Image className='mr-4' height={55} src={UbiquitiLogo} alt="Ubiquiti Logo" />
-				<div>Devices</div>
-			</div>
-			<div className='justify-self-end pr-8'>Scott Cook</div>
-		</nav> 
-	);
+  return (
+    <nav className="flex justify-between items-center text-neutral-600 background-color--light">
+      <div className="flex flex-row items-center">
+        <Image
+          className="mr-4"
+          height={55}
+          src={UbiquitiLogo}
+          alt="Ubiquiti Logo"
+        />
+        <div>Devices</div>
+      </div>
+      <div className="justify-self-end pr-8">Scott Cook</div>
+    </nav>
+  );
 }
 
 export default NavBar;
