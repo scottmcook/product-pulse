4804adc2b74ec1b7f539dcb7262d4a1069ce62ee
scottmcook <scott.madison.cook@gmail.com>
2023-08-22 15:33:00 -0700

    Add props to receive data

--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -1,48 +1,45 @@
 import React from "react";
+import Image from "next/image";
 
-function ProductTable() {
-  let devices = 123;
+interface ProductTable {
+  name: string;
+  imageUrl: string;
+  productLine: string;
+}
 
+function ProductTable(props: ProductTable) {
   return (
-    <table className="table-auto">
-      <colgroup>
-        <col span={1} className=""></col>
-        <col span={1} className=""></col>
-        <col span={1} className="w-2/3"></col>
-      </colgroup>
-      <thead className="text-left uppercase">
-        <tr>
-          <th>{devices}</th>
-          <th>Product Line</th>
-          <th>Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <img
-              alt="poip"
-              src="https://static.ui.com/fingerprint/ui/icons/0b6d0790-dea7-47a0-a1a6-929b85b0f247_25x25.png"
-            ></img>
-          </td>
-          <td>airMAX</td>
-          <td>airCube ISP</td>
-        </tr>
-        <tr>
-          <td>Witchy Woman</td>
-          <td>The Eagles</td>
-          <td>1972</td>
-        </tr>
-        <tr>
-          <td>Shining Star</td>
-          <td>
-            Earth, Wind, and Fire Earth, Wind, and Fire Earth, Wind, and Fire
-            Earth, Wind, and Fire
-          </td>
-          <td>1975</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="grid mt-6 mx-14">
+      <table className="table-auto">
+        <colgroup>
+          <col span={1} className=""></col>
+          <col span={1} className=""></col>
+          <col span={1} className="w-2/3"></col>
+        </colgroup>
+        <thead className="text-left uppercase">
+          <tr>
+            <th># devices</th>
+            <th>Product Line</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Image
+                className=""
+                alt={props.name}
+                height={25}
+                width={25}
+                src={props.imageUrl}
+              />
+            </td>
+            <td>{props.productLine}</td>
+            <td>{props.name}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
