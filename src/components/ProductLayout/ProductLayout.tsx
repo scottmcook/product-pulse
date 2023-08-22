e741f72b6833791fb2b748c81f77226a9e06e892
scottmcook <scott.madison.cook@gmail.com>
2023-08-22 15:32:46 -0700

    Add conditional props

--- src/components/ProductLayout/ProductLayout.tsx
+++ src/components/ProductLayout/ProductLayout.tsx
@@ -1,20 +1,26 @@
 "use client";
 import React from "react";
+
+import ProductToolbar from "../ProductToolbar/ProductToolbar";
 import ProductCard from "../ProductCard/ProductCard";
 import ProductTable from "../ProductTable/ProductTable";
 
 // Data fetching
 import useSWR from "swr";
 
-function ProductLayout() {
-  let isTable = false;
+interface ProductLayout {
+  isTable: boolean;
+}
+
+function ProductLayout(props: ProductLayout) {
+  props.isTable = false;
 
   const UIDB_URL = "https://static.ui.com/fingerprint/ui/public.json";
   const UIDB_IMAGE_URL = "https://static.ui.com/fingerprint/ui/icons";
   const fetcher = async (url: RequestInfo | URL) =>
     fetch(url).then((res) => res.json());
-  const { data, error } = useSWR(UIDB_URL, fetcher);
 
+  const { data, error } = useSWR(UIDB_URL, fetcher);
   if (error)
     return (
       <div>
@@ -29,33 +35,36 @@ function ProductLayout() {
       </div>
     );
 
-  if (isTable) {
-    return (
-      <div className="grid mt-6 mx-14">
-        <ProductTable />
-      </div>
-    );
-  }
   return (
     <>
-      <div className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-1 gap-5 mt-6 mx-14">
-        {data &&
-          data.devices.map(
-            (device: {
-              id: React.Key | null | undefined;
-              product: { name: string };
-              line: { name: string };
-              icon: { resolutions: any[]; id: string };
-            }) => (
-              <ProductCard
-                key={device.id}
-                name={device.product.name}
-                productType={device.line.name}
-                imageUrl={`${UIDB_IMAGE_URL}/${device.icon.id}_257x257.png`}
-              />
-            )
-          )}
-      </div>
+      <ProductToolbar />
+      {data &&
+        data.devices.map(
+          (device: {
+            id: React.Key | null | undefined;
+            product: { name: string };
+            line: { name: string };
+            icon: { resolutions: number[]; id: string };
+          }) => (
+            <>
+              {!props.isTable ? (
+                <ProductCard
+                  key={device.id}
+                  name={device.product.name}
+                  productType={device.line.name}
+                  imageUrl={`${UIDB_IMAGE_URL}/${device.icon.id}_257x257.png`}
+                />
+              ) : (
+                <ProductTable
+                  key={device.id}
+                  name={device.product.name}
+                  productLine={device.line.name}
+                  imageUrl={`${UIDB_IMAGE_URL}/${device.icon.id}_25x25.png`}
+                />
+              )}
+            </>
+          )
+        )}
     </>
   );
 }
