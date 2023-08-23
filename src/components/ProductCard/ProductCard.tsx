e5e9672cd89cac03a56bb797fa4e6eab3ef3e72b
scottmcook <scott.madison.cook@gmail.com>
2023-08-23 10:35:50 -0700

    Add hover scale styles

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -9,7 +9,7 @@ interface Product {
 
 function ProductCard(props: Product) {
   return (
-    <div className="flex flex-col border border-[#dbdce1] rounded-lg">
+    <div className="flex flex-col border border-[#dbdce1] rounded-lg hover:scale-110 transition duration-200 cursor-pointer object-cover">
       <div className="flex justify-center w-full background-color--light">
         <Image
           className=""
