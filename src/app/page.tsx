c414efdf3f76edca983fb3a5835ac32efde9ba0c
scottmcook <scott.madison.cook@gmail.com>
2023-08-30 19:54:21 -0700

    Remove unnecessary comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-
-
-
 export default async function Home() {
-  // const res = await fetch('https://static.ui.com/fingerprint/ui/public.json');
-  // const data = await res.json();
-  // const listItems = data.devices.map((device) =>
-  //   <li key={device.id}>{device.product.name}</li>
-  // );
-
-  return (
-    <main>
-      {/* <ul>{listItems}</ul> */}
-    </main>
-  )
+  return <main>{/* <ul>{listItems}</ul> */}</main>;
 }
